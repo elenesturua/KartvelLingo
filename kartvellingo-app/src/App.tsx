24fde05585e8e96f04696c742b85b0c0d6b7efc6
Elene Sturua <sturuael@grinnell.edu>
2025-07-20 18:45:02 -0500

    fixed layout to be center on the main page

---
+++ kartvellingo-app/src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+    return (
+        <div style={{textAlign: "center", marginTop: "2rem" }}>
+            <h1>Kartvellingo</h1>
+            <img
+                src="/georgianFlag.webp"
+                alt="Georgian flag"
+                style={{ width: "100px", margin: "1rem 0" }}
+            />
+            <p>Learn Georgian with flashcards!</p>
+        </div>
+    );
+}
+
+export default App;

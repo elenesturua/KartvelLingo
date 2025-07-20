4b182133b3b5f8296d21ded52ae342bd8879c876
Elene Sturua <sturuael@grinnell.edu>
2025-07-20 18:53:56 -0500

    added fun catchphrase introduxtion and created a css file for styling

--- kartvellingo-app/src/App.tsx
+++ kartvellingo-app/src/App.tsx
@@ -1,13 +1,22 @@
 function App() {
     return (
         <div style={{textAlign: "center", marginTop: "2rem" }}>
-            <h1>Kartvellingo</h1>
+            <h1>KartvelLingo</h1>
             <img
                 src="/georgianFlag.webp"
                 alt="Georgian flag"
                 style={{ width: "100px", margin: "1rem 0" }}
             />
-            <p>Learn Georgian with flashcards!</p>
+            <p style={{ fontSize: "1.1rem", marginBottom: "1.5rem" }}>
+                Learn Georgian with flashcards!
+            </p>
+
+            <p style={{ fontSize: "1rem", lineHeight: "1.6" }}>
+                Here’s a fun fact! There’s a small country called Georgia, and no, not the U.S. state.
+                We’ve got our own alphabet, culture, and a super unique language that’s not related to any other!
+                Want to pick up a new party trick or surprise your Georgian friends? Start your journey here,
+                and while you are at it, check out the beautiful flag of Georgia above!
+            </p>
         </div>
     );
 }

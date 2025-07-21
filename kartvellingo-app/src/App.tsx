7671f6d3c9f3e51140ccc1668dd5dba79173304a
Elene Sturua <sturuael@grinnell.edu>
2025-07-20 19:25:28 -0500

    - designed the index tab by adding a button that will redirect the learner to next page - adjusted coloring, border for description and added more css elements to enhance the tab

--- kartvellingo-app/src/App.tsx
+++ kartvellingo-app/src/App.tsx
@@ -1,23 +1,30 @@
 import './app.css';
+import {useNavigate} from "react-router-dom";
 
 function App() {
+    const navigate = useNavigate();
+    const handleClick = () => {navigate('/plans');};
     return (
         <div className="app-container">
             <h1>KartvelLingo</h1>
             <img
                 src="/georgianFlag.webp"
                 alt="Georgian flag"
+                className="flag-image"
             />
-            <p>
+            <div className="card">
+            <p className="subtitle">
                 Learn Georgian with flashcards!
             </p>
 
-            <p>
+            <p className="description">
                 Here’s a fun fact! There’s a small country called Georgia, and no, not the U.S. state.
                 We’ve got our own alphabet, culture, and a super unique language that’s not related to any other!
                 Want to pick up a new party trick or surprise your Georgian friends? Start your journey here,
                 and while you are at it, check out the beautiful flag of Georgia above!
             </p>
+                <button className="start-button" onClick={handleClick}>Start Learning </button>
+        </div>
         </div>
     );
 }

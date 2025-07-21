77cb993f3f176370c41cb5ac0321dd867d348328
Elene Sturua <sturuael@grinnell.edu>
2025-07-20 21:04:20 -0500

    - added letters and possible english equivalents to the letters tab

---
+++ kartvellingo-app/src/letters.tsx
@@ -0,0 +1,56 @@
+function letters() {
+    return (
+        <div className={"letters-page"}>
+            <h2>Georgian Alphabet</h2>
+            <p>Here are all 33 Georgian Letters from the Georgian alphabet</p>
+
+        <ul className={"letter-list"}>
+            <li>ა - A</li>
+            <li>ბ - B</li>
+            <li>გ - G</li>
+            <li>დ - D</li>
+            <li>ე - E</li>
+            <li>ვ - V</li>
+            <li>ზ - Z</li>
+            <li>თ - T</li>
+            <li>ი - I</li>
+            <li>კ - K</li>
+            <li>ლ - L</li>
+            <li>მ - M</li>
+            <li>ნ - N</li>
+            <li>ო - O</li>
+            <li>პ - P</li>
+            <li>ჟ - ZH?</li>
+            <li>რ - R</li>
+            <li>ს - S</li>
+            <li>ტ - T</li>
+            <li>უ - U</li>
+            <li>ფ - F</li>
+            <li>ქ - Q</li>
+            <li>ღ - GH</li>
+            <li>ყ - K</li>
+            <li>შ - SH</li>
+            <li>ჩ - CH</li>
+            <li>ც - C</li>
+            <li>ძ - DZ</li>
+            <li>წ - TS</li>
+            <li>ჭ - CH?</li>
+            <li>ხ - KH</li>
+            <li>ჯ - J</li>
+            <li>ჰ - H</li>
+
+
+
+
+
+
+
+
+
+        </ul>
+
+
+        </div>
+    );
+}
+export default letters;
--- kartvellingo-app/src/main.tsx
+++ kartvellingo-app/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
 import Plans from './Plans.tsx';
+import Letters from './letters.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/plans" element={<Plans />} />
+                <Route path="/letters" element={<Letters />} />
             </Routes>
         </BrowserRouter>
     </StrictMode>

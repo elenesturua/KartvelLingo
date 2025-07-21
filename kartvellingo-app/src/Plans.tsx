0e99b993369c6bb8badc852262fe68476e74e62d
Elene Sturua <sturuael@grinnell.edu>
2025-07-20 21:59:46 -0500

    - added two more learning plans options: basic phrases & fun facts about the language

--- kartvellingo-app/src/Plans.tsx
+++ kartvellingo-app/src/Plans.tsx
@@ -9,9 +9,17 @@ function Plans() {
             <h2>Your Journey Begins Here!! </h2>
             <p>Select a category to start learning:</p>
             <div className={"plan-card"} onClick={handleStartLetters}>
-                <h3> Let's learn the Georgian Alphabet </h3>
+                <h3> the Georgian Alphabet </h3>
                 <button className={"start-button"}>Start Now</button>
         </div>
+            <div className={"plan-card"} onClick={handleStartLetters}>
+                <h3> Fun Facts </h3>
+                <button className={"start-button"}>Start Now</button>
+            </div>
+            <div className={"plan-card"} onClick={handleStartLetters}>
+                <h3> Basic Phrases </h3>
+                <button className={"start-button"}>Start Now</button>
+            </div>
             </div>
     );
 }

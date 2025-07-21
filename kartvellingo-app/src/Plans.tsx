de6cda9340843ff3e8402f6037a6e36cc79607c5
Elene Sturua <sturuael@grinnell.edu>
2025-07-20 20:53:43 -0500

    -designed the tab intro where the users will learn the letters

--- kartvellingo-app/src/Plans.tsx
+++ kartvellingo-app/src/Plans.tsx
@@ -1,10 +1,19 @@
+import {useNavigate} from "react-router-dom";
+
+
 function Plans() {
+    const navigate = useNavigate();
+    const handleStartLetters = () => {navigate('/letters');};
     return (
-        <div style={{ padding: '3rem', textAlign: 'center' }}>
+        <div className={"plans-container"}>
             <h2>Your Journey Begins Here!! </h2>
             <p>Select a category to start learning:</p>
-            { }
+            <div className={"plan-card"} onClick={handleStartLetters}>
+                <h3> Let's learn the Georgian Alphabet </h3>
+                <p>Here are the Georgian Letters in their script and their closest variants in Latin script</p>
+                <button className={"start-button"}>Start Now </button>
         </div>
+            </div>
     );
 }
 
